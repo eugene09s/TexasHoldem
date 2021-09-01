4f9de2a796c3c50f6e702ea8fad57c88b8829133
Evgenij009 <evgeshashadura@gmail.com>
2021-09-01 22:32:10 +0300

    Big changes. Delete tables roles and statuses. Exchange some values tables users and profile_players

--- src/sql/setup.sql
+++ src/sql/setup.sql
@@ -2,20 +2,6 @@
 CREATE SCHEMA IF NOT EXISTS `pokerschema` DEFAULT CHARACTER SET utf8 ;
 USE `pokerschema` ;
 
-
-CREATE TABLE IF NOT EXISTS `pokerschema`.`roles` (
-                                                     `role_id` BIGINT NOT NULL AUTO_INCREMENT,
-                                                     `role` ENUM('ADMIN', 'USER', 'MANAGER', 'GUEST') NOT NULL,
-                                                     PRIMARY KEY (`role_id`),
-                                                     UNIQUE INDEX `role_UNIQUE` (`role` ASC) VISIBLE);
-
-
-CREATE TABLE IF NOT EXISTS `pokerschema`.`statuses` (
-                                                        `status_id` BIGINT NOT NULL AUTO_INCREMENT,
-                                                        `status` ENUM('ACTIVE', 'BANNED', 'CONFIRMATION_AWAITING') NOT NULL,
-                                                        PRIMARY KEY (`status_id`));
-
-
 CREATE TABLE IF NOT EXISTS `pokerschema`.`users` (
                                                      `user_id` BIGINT NOT NULL AUTO_INCREMENT,
                                                      `login` VARCHAR(32) NOT NULL,
@@ -24,29 +10,14 @@ CREATE TABLE IF NOT EXISTS `pokerschema`.`users` (
                                                      `last_name` VARCHAR(32) NOT NULL,
                                                      `email` VARCHAR(64) NOT NULL,
                                                      `balance` DECIMAL(10,2) NOT NULL DEFAULT 100,
-                                                     `photo` BLOB NULL,
-                                                     `create_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                                                     `phone_number` VARCHAR(16) NULL DEFAULT '0000000000000000',
-                                                     `about_yourself` VARCHAR(512) NULL DEFAULT 'write about yourself',
-                                                     `user_role_id` BIGINT NOT NULL,
-                                                     `user_status_id` BIGINT NOT NULL,
+                                                     `role` ENUM('ADMIN', 'USER', 'MANAGER', 'GUEST') NOT NULL,
+                                                     `status` ENUM('ACTIVE', 'BANNED', 'CONFIRMATION_AWAITING') NOT NULL,
+                                                     `phone_number` BIGINT NULL DEFAULT 0,
+                                                     `create_time` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
                                                      PRIMARY KEY (`user_id`),
                                                      UNIQUE INDEX `login_UNIQUE` (`login` ASC) VISIBLE,
-                                                     INDEX `status_id_idx` (`user_status_id` ASC) VISIBLE,
-                                                     INDEX `role_id_idx` (`user_role_id` ASC) VISIBLE,
                                                      UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE,
-                                                     UNIQUE INDEX `user_id_UNIQUE` (`user_id` ASC) VISIBLE,
-                                                     CONSTRAINT `role_id`
-                                                         FOREIGN KEY (`user_role_id`)
-                                                             REFERENCES `pokerschema`.`roles` (`role_id`)
-                                                             ON DELETE CASCADE
-                                                             ON UPDATE CASCADE,
-                                                     CONSTRAINT `status_id`
-                                                         FOREIGN KEY (`user_status_id`)
-                                                             REFERENCES `pokerschema`.`statuses` (`status_id`)
-                                                             ON DELETE CASCADE
-                                                             ON UPDATE CASCADE);
-
+                                                     UNIQUE INDEX `user_id_UNIQUE` (`user_id` ASC) VISIBLE);
 
 CREATE TABLE IF NOT EXISTS `pokerschema`.`games` (
                                                      `game_id` BIGINT NOT NULL AUTO_INCREMENT,
@@ -57,14 +28,13 @@ CREATE TABLE IF NOT EXISTS `pokerschema`.`games` (
                                                      PRIMARY KEY (`game_id`),
                                                      UNIQUE INDEX `game_id_UNIQUE` (`game_id` ASC) VISIBLE);
 
-
 CREATE TABLE IF NOT EXISTS `pokerschema`.`game_winners` (
-                                                            game_winner_id BIGINT NOT NULL AUTO_INCREMENT,
+                                                            `game_winner_id` BIGINT NOT NULL AUTO_INCREMENT,
                                                             `game_id` BIGINT NOT NULL,
                                                             `game_winner_user_id` BIGINT NOT NULL,
                                                             INDEX `player_user_id_idx` (`game_winner_user_id` ASC) VISIBLE,
-                                                            PRIMARY KEY (game_winner_id),
-                                                            UNIQUE INDEX `game_winners_id_UNIQUE` (game_winner_id ASC) VISIBLE,
+                                                            PRIMARY KEY (`game_winner_id`),
+                                                            UNIQUE INDEX `game_winners_id_UNIQUE` (`game_winner_id` ASC) VISIBLE,
                                                             CONSTRAINT `game_user_id`
                                                                 FOREIGN KEY (`game_id`)
                                                                     REFERENCES `pokerschema`.`games` (`game_id`)
@@ -76,20 +46,20 @@ CREATE TABLE IF NOT EXISTS `pokerschema`.`game_winners` (
                                                                     ON DELETE CASCADE
                                                                     ON UPDATE CASCADE);
 
-
-CREATE TABLE IF NOT EXISTS `pokerschema`.`rating_players` (
-                                                              `player_id` BIGINT NOT NULL AUTO_INCREMENT,
-                                                              `ranking` DECIMAL(4,2) NOT NULL DEFAULT 0,
-                                                              `best_prize` DECIMAL(10,2) NOT NULL DEFAULT 0,
-                                                              `award` VARCHAR(128) NULL DEFAULT NULL,
-                                                              UNIQUE INDEX `player_id_UNIQUE` (`player_id` ASC) VISIBLE,
-                                                              PRIMARY KEY (`player_id`),
-                                                              CONSTRAINT `player_id`
-                                                                  FOREIGN KEY (`player_id`)
-                                                                      REFERENCES `pokerschema`.`users` (`user_id`)
-                                                                      ON DELETE CASCADE
-                                                                      ON UPDATE CASCADE);
-
+CREATE TABLE IF NOT EXISTS `pokerschema`.`profile_players` (
+                                                               `player_id` BIGINT NOT NULL AUTO_INCREMENT,
+                                                               `ranking` DECIMAL(4,2) NOT NULL DEFAULT 0,
+                                                               `best_prize` DECIMAL(10,2) NOT NULL DEFAULT 0,
+                                                               `award` VARCHAR(128) NULL DEFAULT NULL,
+                                                               `photo` BLOB NULL DEFAULT NULL,
+                                                               `about_yourself` VARCHAR(512) NULL DEFAULT NULL,
+                                                               UNIQUE INDEX `player_id_UNIQUE` (`player_id` ASC) VISIBLE,
+                                                               PRIMARY KEY (`player_id`),
+                                                               CONSTRAINT `player_id`
+                                                                   FOREIGN KEY (`player_id`)
+                                                                       REFERENCES `pokerschema`.`users` (`user_id`)
+                                                                       ON DELETE CASCADE
+                                                                       ON UPDATE CASCADE);
 
 CREATE TABLE IF NOT EXISTS `pokerschema`.`game_players` (
                                                             `game_player_id` BIGINT NOT NULL AUTO_INCREMENT,
