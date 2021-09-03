0122d2430357bd975db6c82d7bd2ec89d20baab8
Evgenij009 <evgeshashadura@gmail.com>
2021-09-03 23:24:08 +0300

    user validator is already

---
+++ src/main/java/com/epam/poker/logic/validator/impl/UserValidator.java
@@ -0,0 +1,64 @@
+package com.epam.poker.logic.validator.impl;
+
+import com.epam.poker.logic.validator.Validator;
+import com.epam.poker.model.entity.User;
+import com.epam.poker.model.enumeration.UserRole;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserValidator implements Validator<User> {
+    private static final int MAX_FIELD_USER_LENGTH = 32;
+    private static final int MIN_FIELD_USER_LENGTH = 3;
+    private static final int MIN_LOGIN_USER_LENGTH = 8;
+    private static final int MAX_EMAIL_LENGTH = 64;
+    private static final int MAX_PHONE_NUMBER_LENGTH = 18;
+    private static final String EMAIL_PATTERN = "^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}";
+    private static final Pattern COMPILED_PATTERN_EMAIL = Pattern.compile(EMAIL_PATTERN);
+    private static final String NAME_PATTERN = "[A-zА-я]+";
+    private static final Pattern COMPILED_PATTERN_NAME = Pattern.compile(NAME_PATTERN);
+
+    @Override
+    public boolean isValid(User user) {
+        String login = user.getLogin();
+        String password = user.getPassword();
+        String firstName = user.getFirstName();
+        String lastName = user.getLastName();
+        String email = user.getEmail();
+        UserRole userRole = user.getUserRole();
+        String phoneNumber = String.valueOf(user.getPhoneNumber());
+        if (login == null || password == null || firstName == null
+                || lastName == null || email == null || userRole == null) {
+            return false;
+        }
+        if (phoneNumber.length() > MAX_PHONE_NUMBER_LENGTH) {
+            return false;
+        }
+        if (login.isEmpty() || login.length() > MAX_FIELD_USER_LENGTH) {
+            return false;
+        }
+        if (password.isEmpty() || password.length() > MAX_FIELD_USER_LENGTH) {
+            return false;
+        }
+        if (firstName.isEmpty() || firstName.length() > MAX_FIELD_USER_LENGTH || isValidName(firstName)) {
+            return false;
+        }
+        if (lastName.isEmpty() || lastName.length() > MAX_FIELD_USER_LENGTH || isValidName(lastName)) {
+            return false;
+        }
+        if (email.isEmpty() || email.length() > MAX_EMAIL_LENGTH || isValidEmail(email)) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isValidEmail(String email) {
+        Matcher matcher = COMPILED_PATTERN_EMAIL.matcher(email);
+        return matcher.matches();
+    }
+
+    private boolean isValidName(String name) {
+        Matcher matcher = COMPILED_PATTERN_NAME.matcher(name);
+        return matcher.matches();
+    }
+}
