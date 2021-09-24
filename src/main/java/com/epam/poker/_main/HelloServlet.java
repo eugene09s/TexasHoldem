906393bc0f2752b562e0eaadfad17a3a5c5c0f1e
Evgenij009 <evgeshashadura@gmail.com>
2021-09-24 22:22:23 +0300

    test max age cookie

--- src/main/java/com/epam/poker/_main/HelloServlet.java
+++ src/main/java/com/epam/poker/_main/HelloServlet.java
@@ -1,6 +1,8 @@
 package com.epam.poker._main;
 
 import java.io.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
@@ -20,21 +22,22 @@ public class HelloServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
         c++;
-        LOGGER.info("Value info c: " + c);
-        LOGGER.warn("Value warn c: " + c);
-        LOGGER.fatal("Value warn c: " + c);
-
-        try {
-            int i = 2;
-            i = i / 0;
-        } catch (Exception e) {
-            LOGGER.error(e);
-        }
+        Cookie[] cookies = request.getCookies();
         // Hello
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
         out.println("<h1>" + message + c + "</h1>");
+        LocalDateTime exp = LocalDateTime.now();
+        out.println("<h2>" + exp + "</h2>");
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                out.println("<p>"+ "MaxAge: " + cookie.getMaxAge() + "</p><br>");
+            }
+        }
         out.println("</body></html>");
+        Cookie cookie = new Cookie("Key" + c, String.valueOf("Number-"+ c));
+        cookie.setMaxAge(60*60*3 + 2*60);
+        response.addCookie(cookie);
     }
 
     public void destroy() {
